e032eb1684855dba5ba934cf0d61ce30d99f8b65
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-07-08 14:59:12 +0200

    add primeng, angular router, primeflex and configs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomePage} from "./pages/home/home.page";
+import { HomeButtonComponent } from './components/home-button/home-button.component';
+import {CardModule} from "primeng/card";
+import { ButtonModule} from "primeng/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    HomeButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
