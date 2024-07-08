bbfdce07a8baa82f03b07002cae2c998000b052b
Your Name <you@example.com>
2024-07-08 11:59:55 +0200

    wip route auth ( register good / login wip )

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,9 +1,11 @@
 import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
+  matchPassword(enteredPassword: string): Promise<boolean>;
 }
 
 const UserSchema: Schema = new Schema({
@@ -12,4 +14,12 @@ const UserSchema: Schema = new Schema({
   password: { type: String, required: true },
 });
 
+UserSchema.methods.matchPassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 export const User = mongoose.model<IUser>("User", UserSchema);
+
+export default User;
