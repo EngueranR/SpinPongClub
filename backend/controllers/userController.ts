2570ed428cad90afff864f7b896a13236597ac73
Your Name <you@example.com>
2024-07-08 22:17:31 +0200

    route update

---
+++ backend/controllers/userController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+
+interface CustomRequest extends Request {
+  userId?: string;
+}
+
+export const updateUser = async (req: CustomRequest, res: Response) => {
+  const { userId } = req; // Utilisation de userId depuis req
+  const { email, username, password } = req.body;
+
+  if (!userId) {
+    return res.status(400).json({ message: "ID utilisateur non fourni" });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    console.log(user);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (email) user.email = email;
+    if (username) user.username = username;
+    if (password) user.password = await bcrypt.hash(password, 10); // Hash le nouveau mot de passe
+
+    await user.save();
+
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,11 @@
 import app from "./app";
 import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
 
 const PORT = process.env.PORT || 9000;
 
 app.use("/routes/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
