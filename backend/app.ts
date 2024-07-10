d49a876c284c8f9a7c94226b46d26eff29f7f628
Your Name <you@example.com>
2024-07-10 09:10:59 +0200

    route tournament + controller

--- backend/app.ts
+++ backend/app.ts
@@ -4,6 +4,8 @@ import connectDB from "./config/db";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import contactRoutes from "./routes/contactRoutes";
+import newsRoutes from "./routes/newsRoutes";
+import tournamentsRoutes from "./routes/tournamentsRoutes";
 
 const app = express();
 
@@ -17,5 +19,7 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/contact", contactRoutes);
+app.use("/api/news", newsRoutes);
+app.use("/api/tournaments", tournamentsRoutes);
 
 export default app;
--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,7 @@ import userRoutes from "./routes/userRoutes";
 import contactRoutes from "./routes/contactRoutes";
 import newsRoutes from "./routes/newsRoutes";
 import cors from "cors";
+import tournamentsRoutes from "./routes/tournamentsRoutes";
 
 const corsOptions = {
   origin: "http://localhost:4200", // Remplacez cette URL par celle de votre frontend
@@ -17,6 +18,7 @@ app.use("/routes/auth", authRoutes);
 app.use("/routes/user", userRoutes);
 app.use("/routes/contact", contactRoutes);
 app.use("/routes/news", newsRoutes);
+app.use("/routes/tournaments", tournamentsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
