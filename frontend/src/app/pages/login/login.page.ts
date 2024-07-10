5e920c1effe1e9b68141edf5de377f4ef4bb44be
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-07-10 08:22:34 +0200

    Fix login page

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
