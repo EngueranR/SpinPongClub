2c8859fef8f0c68d5b8f8b1ddf4e9137f0ba2184
Your Name <you@example.com>
2024-07-10 08:45:27 +0200

    routes news

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import app from "./app";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import contactRoutes from "./routes/contactRoutes";
+import newsRoutes from "./routes/newsRoutes";
 import cors from "cors";
 
 const corsOptions = {
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 9000;
 app.use("/routes/auth", authRoutes);
 app.use("/routes/user", userRoutes);
 app.use("/routes/contact", contactRoutes);
+app.use("/routes/news", newsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
