9a7dce364a75538d29a8d36435b78c1a3dcf465d
Your Name <you@example.com>
2024-07-10 08:25:22 +0200

    cors

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,14 @@ import app from "./app";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import contactRoutes from "./routes/contactRoutes";
+import cors from "cors";
 
+const corsOptions = {
+  origin: "http://localhost:4200", // Remplacez cette URL par celle de votre frontend
+  optionsSuccessStatus: 200, // pour les navigateurs plus anciens
+};
+
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 9000;
 
 app.use("/routes/auth", authRoutes);
