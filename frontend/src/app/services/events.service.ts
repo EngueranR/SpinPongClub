4552f35636c8c1bdb741c9a9d7c230120017a435
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-07-11 18:40:01 +0200

    Add model and service for events

---
+++ frontend/src/app/services/events.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Event} from "../models/event";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  private baseUrl: string = 'http://localhost:9000/api/events';
+
+  constructor(private http: HttpClient) {}
+
+  getEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(`${this.baseUrl}`);
+  }
+
+  addEvent(event: Event): Observable<Event> {
+    return this.http.post<Event>(`${this.baseUrl}`, event);
+  }
+
+  deleteEvent(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+
+  editEvent(id: string, event: Event): Observable<Event> {
+    return this.http.put<Event>(`${this.baseUrl}/${id}`, event);
+  }
+}
